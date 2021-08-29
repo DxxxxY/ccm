c5964e3df4b50ed10209974b4908ce5ed338412b
DxxxxY <introsdream@gmail.com>
2021-08-29 14:17:12 -0400

    readme, merge inject and download, help and bypass

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # CCM
+CLI for the CS:GO cheating community. Makes your life easier.
+
+## Node Dependencies
+> Install these using <kbd>npm i</kbd>
+```js
+├── @octokit/auth-basic@1.4.8
+├── @octokit/rest@18.9.1
+├── chalk@4.1.2
+├── clear@0.1.0
+├── clui@0.3.6
+├── configstore@6.0.0
+├── download@8.0.0
+├── figlet@1.5.2
+├── inquirer@8.1.2
+├── lodash@4.17.21
+├── minimist@1.2.5
+├── node-fetch@2.6.1
+├── python-shell@3.0.0
+├── request@2.88.2
+├── simple-git@2.45.0
+├── tabletojson@2.0.7
+└── touch@3.1.0
+```
+
+## Commands
+> There are numerous aliases to the commands, for an example, the first letter of every command.
+```
+ccm ── Main command / About
+ccm help ── Help
+ccm list ── List of all cheats hosted online
+ccm open ── Opens CS:GO
+ccm bypass ── Launches VAC Bypass (may need to be launched twice)
+ccm inject <cheat> ── Downloads and injects the selected cheat
+```
+
+## Credits
+> This project was possible using the help of these programs.
+
+Injector ── modified version of [martinjanas/RandoInjector](https://github.com/martinjanas/RandoInjector)
+
+Bypass ── [danielkrupinski/VAC-Bypass-Loader](https://github.com/danielkrupinski/VAC-Bypass-Loader)
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const download = require('download')
 const tabletojson = require('tabletojson').Tabletojson
 const fetch = require("node-fetch")
 const fs = require("fs")
-const { exec, execSync, spawn, spawnSync } = require("child_process")
+const { exec, execSync } = require("child_process")
 
 const list = () => {
     fetch("https://dreamysoft.net/ccm")
@@ -19,7 +19,6 @@ const list = () => {
         })
 }
 
-//maybe await is needed
 const checkOnline = async() => {
     let found = false
     await fetch("https://dreamysoft.net/ccm")
@@ -33,30 +32,42 @@ const checkOnline = async() => {
     return found
 }
 
+const inject = async() => {
+    if (await checkOnline()) console.log(`Found [${chalk.cyanBright(process.argv[3])}]`)
+    else return console.log(`[${chalk.redBright(process.argv[3])}] not found\nType ${chalk.cyanBright("ccm list")} to view all cheats available for download`)
+    console.log(`Downloading [${chalk.cyanBright(process.argv[3])}]...`)
+        //if (fs.existsSync(`${__dirname}/files/${process.argv[3]}`)) fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`)
+    download(`https://dreamysoft.net/ccm/${process.argv[3]}`, `${__dirname}/files`).then(() => {
+        console.log(`Downloaded [${chalk.cyanBright(process.argv[3])}]`)
+        console.log(`Downloading [${chalk.cyanBright("Injector")}...`)
+        download(`https://dreamysoft.net/utils/RandoInjector.exe`, `${__dirname}/files`).then(() => {
+            console.log(`Downloaded [${chalk.cyanBright("Injector")}`)
+            console.log(`Injecting...`)
+            execSync(`start ${__dirname}/files/RandoInjector.exe ${__dirname}/files/${process.argv[3]}`, [], { shell: true })
+            fs.unlinkSync(`${__dirname}/files/RandoInjector.exe`)
+                //fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`)
+        })
+    }).catch(err => console.log(err))
+}
+
 switch (process.argv[2]) {
     case "h":
     case "help":
-        console.log("ccm <command>\n\nUsage:\n\nccm down <cheat> - downloads the selected cheat\nccm list - shows the name of all cheats available to download\nccm help - shows a help page for beginners")
+        console.log(`
+ccm ── Main command / About
+ccm help ── Help
+ccm list ── List of all cheats hosted online
+ccm down <cheat> ── Downloads the selected cheat
+ccm open ── Opens CS:GO
+ccm bypass ── Launches VAC Bypass (may need to be launched twice)
+ccm inject ── Injects the selected cheat (needs to be downloaded first)`)
         break
     case "l":
     case "list":
         console.log("Retrieving list...")
         list()
         break
-    case "d":
-    case "down":
-    case "download":
-        (async() => {
-            if (!process.argv[3]) return console.log(`Missing <${chalk.redBright("cheat")}> argument`)
-            if (await checkOnline()) console.log(`Found [${chalk.cyanBright(process.argv[3])}]`)
-            else return console.log(`[${chalk.redBright(process.argv[3])}] not found\nType ${chalk.cyanBright("ccm list")} to view all cheats available for download`)
-            console.log(`Downloading [${chalk.cyanBright(process.argv[3])}]...`)
-            download(`https://dreamysoft.net/ccm/${process.argv[3]}`, `${__dirname}/files`).then(() => {
-                console.log(`Downloaded [${chalk.cyanBright(process.argv[3])}]`)
-            }).catch(err => console.log(err))
-        })()
-        break
-    case "csgo":
+    case "o":
     case "open":
         console.log(`Launching ${chalk.cyanBright("[CS:GO]")}...`)
         exec("start steam://rungameid/730")
@@ -64,35 +75,19 @@ switch (process.argv[2]) {
         break
     case "b":
     case "bypass":
-    case "vac":
+        console.log(`Downloading [${chalk.cyanBright("VAC Bypass")}]...`)
         download(`https://dreamysoft.net/utils/VAC-Bypass-Loader.exe`, `${__dirname}/files`).then(() => {
-            execSync(`${__dirname}/files/VAC-Bypass-Loader`)
-            fs.unlinkSync(`${__dirname}/files/VAC-Bypass-Loader`)
+            console.log(`Downloaded [${chalk.cyanBright("VAC Bypass")}]`)
+            execSync(`start ${__dirname}/files/VAC-Bypass-Loader.exe`)
+            console.log("If steam does not open, re-execute the command")
+            console.log("You should see a message box informing you of the bypass")
+            fs.unlinkSync(`${__dirname}/files/VAC-Bypass-Loader.exe`)
         })
         break
     case "i":
     case "inject":
-        //return console.log("In Progress")
         if (!process.argv[3]) return console.log(`Missing <${chalk.redBright("cheat")}> argument`)
-        if (fs.existsSync(`${__dirname}/files/${process.argv[3]}`)) {
-            download(`https://dreamysoft.net/utils/PE2HEX.py`, `${__dirname}/files`).then(() => {
-                let child = spawnSync(`start ${__dirname}/files/PE2HEX.py ${__dirname}/files/${process.argv[3]} --out ${__dirname}/files/temp.txt`, [], { shell: true })
-                console.log("1")
-                child.on('exit', () => {
-                    //fs.unlinkSync(`${__dirname}/files/PE2HEX.py`)
-                    console.log("2")
-                })
-                console.log("3")
-                    //console.log("txt")
-
-                //execSync(`start ${__dirname}/files/PE2HEX.py ${__dirname}/files/${process.argv[3]} --out ${__dirname}/files/temp.txt`)
-                // fs.unlinkSync(`${__dirname}/files/PE2HEX.py`)
-                //after its finished download injector and supply temp.txt
-            })
-        } else return console.log(`[${chalk.redBright(process.argv[3])}] not found\nMake sure you have downloaded it first`)
-
-        //check if cheat exists
-        //download memject.exe and launch with parameter
+        inject()
         break
     default:
         console.log(`Welcome to ${chalk.cyanBright("ccm b1.0.0")}\nType ${chalk.cyanBright("ccm help")} for more information`)
