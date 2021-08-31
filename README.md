468eed221464b861e1fd3529221be4701c6b0d8a
DxxxxY <introsdream@gmail.com>
2021-08-31 17:09:52 -0400

    error handling, better readme

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # CCM
-CLI for the CS:GO cheating community. Makes your life easier.
+Open-Source CLI for the CS:GO cheating community. Makes your life easier.
+
+## Table of Contents
+[Installing](#installing) |
+[Features](#features) |
+[Commands](#Commands) |
+[Path](#path) |
+[Cheats](#list-of-cheats) |
+[Exceptions](#exceptions) |
+[Credits](#credits)
 
 ## Installing
-
 ```
 npm install ccm-cli -g
 ```
+Notes:
+> You need to start the terminal with administrative permissions.
+
+> You need to add either [path](#path) to your antivirus exclusions.
+
+## Features
+It's safe!
+> Some other loaders like BetaLoader was exposed for being a rat/btc miner and DarkSpy was exposed for being a discord token grabber.
+- It's open-source (no bullshit, it's unobfuscated JavaScript that gets installed to [path](#path) by npm) 
+
+- Cheats are either compiled from github source or taken from yougame.biz (reputable russian cheat forum)
 
 ## Commands
 > There are numerous aliases to the commands, for an example, the first letter of every command.
->> You would probably need to start the terminal with administrator.
-
 ```
 ccm ── Main command / About
 ccm help ── Help
@@ -20,11 +37,38 @@ ccm bypass ── Launches VAC Bypass (may need to be launched twice)
 ccm inject <cheat> ── Downloads and injects the selected cheat
 ```
 
+## Path
+Automatically installs to:
+> Copy and paste this in your file explorer.
+```
+%appdata%/npm/node_modules/ccm-cli
+```
+Cheats, injector and bypass gets downloaded to: 
+> Copy and paste this in your file explorer.
+```
+%appdata%/npm/node_modules/ccm-cli/files
+```
+
+## List of cheats
+> Accessible through the <kbd>ccm l</kbd> command.
+
+> If you ever want me to add a cheat, open an issue on github (must be from somewhere trusted).
+```
+├── Name ── Last modified ── Size
+├── clarity.dll ── 2021-08-29 14:36 ── 1.4M
+├── interium.dll ── 2021-08-29 14:16 ── 10M
+├── nixware.dll ── 2021-08-29 14:28 ── 48M
+├── osiris.dll ── 2021-08-29 13:48 ── 1.3M
+├── otc2.dll ── 2021-08-29 00:50 ── 2.8M
+├── otc3.dll ── 2021-08-28 00:24 ── 8.7M
+├── supremacy.dll ── 2021-08-29 14:42 ── 1.2M
+```
+
+## Exceptions
+- interium.dll ── [download, un-zip and drop this folder in %appdata%](https://mega.nz/file/dc8mCC7C#8qkdalh6m-DBOM1gFeD5tgAj8SJMG0eqgW30gka3UK4) (otherwise, cheat will crash)
+
 ## Credits
 > This project was possible using the help of these programs.
 - Injector ── modified version of [martinjanas/RandoInjector](https://github.com/martinjanas/RandoInjector)
 
 - Bypass ── [danielkrupinski/VAC-Bypass-Loader](https://github.com/danielkrupinski/VAC-Bypass-Loader)
-
-## Exceptions
-- interium.dll ── [download, un-zip and drop this folder in %appdata%](https://mega.nz/file/dc8mCC7C#8qkdalh6m-DBOM1gFeD5tgAj8SJMG0eqgW30gka3UK4) (otherwise, will crash)
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk")
-const download = require('download')
-const tabletojson = require('tabletojson').Tabletojson
+const download = require("download")
+const tabletojson = require("tabletojson").Tabletojson
 const fetch = require("node-fetch")
 const fs = require("fs")
-const { exec, execSync } = require("child_process")
+const execSync = require("child_process").execSync
 
 const list = () => {
     fetch("https://dreamysoft.net/ccm")
@@ -16,7 +16,7 @@ const list = () => {
                 if (obj.Name === "Parent Directory") return
                 console.log(`├── ${obj.Name} ── ${obj["Last modified"]} ── ${obj.Size}`)
             })
-        })
+        }).catch(err => console.log(err))
 }
 
 const checkOnline = async() => {
@@ -28,7 +28,7 @@ const checkOnline = async() => {
                 if (obj.Name === "Parent Directory") return
                 if (obj.Name === process.argv[3]) found = true
             })
-        })
+        }).catch(err => console.log(err))
     return found
 }
 
@@ -37,7 +37,7 @@ const inject = async() => {
     else return console.log(`[${chalk.redBright(process.argv[3])}] not found\nType ${chalk.cyanBright("ccm list")} to view all cheats available for download`)
     if (!fs.existsSync(`${__dirname}/files/${process.argv[3]}`)) {
         console.log(`Downloading [${chalk.cyanBright(process.argv[3])}]...`)
-            //if (fs.existsSync(`${__dirname}/files/${process.argv[3]}`)) fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`)
+            //if (fs.existsSync(`${__dirname}/files/${process.argv[3]}`)) fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`) //test
         download(`https://dreamysoft.net/ccm/${process.argv[3]}`, `${__dirname}/files`).then(() => {
             console.log(`Downloaded [${chalk.cyanBright(process.argv[3])}]`)
             console.log(`Downloading [${chalk.cyanBright("Injector")}]...`)
@@ -45,8 +45,8 @@ const inject = async() => {
                 console.log(`Downloaded [${chalk.cyanBright("Injector")}]`)
                 console.log(`Injecting...`)
                 execSync(`start ${__dirname}/files/RandoInjector.exe ${__dirname}/files/${process.argv[3]}`, [], { shell: true })
-                    //fs.unlinkSync(`${__dirname}/files/RandoInjector.exe`)
-                    //fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`)
+                    //fs.unlinkSync(`${__dirname}/files/RandoInjector.exe`) //test
+                    //fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`) //test
             }).catch(err => console.log(err))
         }).catch(err => console.log(err))
     } else {
@@ -56,8 +56,8 @@ const inject = async() => {
             console.log(`Downloaded [${chalk.cyanBright("Injector")}]`)
             console.log(`Injecting...`)
             execSync(`start ${__dirname}/files/RandoInjector.exe ${__dirname}/files/${process.argv[3]}`, [], { shell: true })
-                //fs.unlinkSync(`${__dirname}/files/RandoInjector.exe`)
-                //fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`)
+                //fs.unlinkSync(`${__dirname}/files/RandoInjector.exe`) //test
+                //fs.unlinkSync(`${__dirname}/files/${process.argv[3]}`) //test
         }).catch(err => console.log(err))
     }
 }
@@ -75,13 +75,13 @@ ccm inject <cheat> ── Downloads and injects the selected cheat`)
         break
     case "l":
     case "list":
-        console.log("Retrieving list...")
+        console.log("Retrieving list from server...")
         list()
         break
     case "o":
     case "open":
         console.log(`Launching [${chalk.cyanBright("CS:GO")}]...`)
-        exec("start steam://rungameid/730")
+        execSync("start steam://rungameid/730")
         console.log(`Launched [${chalk.cyanBright("CS:GO")}]`)
         break
     case "b":
@@ -92,8 +92,8 @@ ccm inject <cheat> ── Downloads and injects the selected cheat`)
             execSync(`start ${__dirname}/files/VAC-Bypass-Loader.exe`)
             console.log("If steam does not open, re-execute the command")
             console.log("You should see a message box informing you of the bypass")
-            fs.unlinkSync(`${__dirname}/files/VAC-Bypass-Loader.exe`)
-        })
+                //fs.unlinkSync(`${__dirname}/files/VAC-Bypass-Loader.exe`)
+        }).catch(err => console.log(err))
         break
     case "i":
     case "inject":
@@ -101,6 +101,6 @@ ccm inject <cheat> ── Downloads and injects the selected cheat`)
         inject()
         break
     default:
-        console.log(`Welcome to ${chalk.cyanBright("ccm b1.0.0")}\nType ${chalk.cyanBright("ccm help")} for more information`)
+        console.log(`Welcome to ${chalk.cyanBright("ccm v1.1.0")}\nType ${chalk.cyanBright("ccm help")} for more information`)
         return
 }
